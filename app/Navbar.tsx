594274f1eedd29c6dacc3f46f9ef246873c64ae1
007saki <saki.baleiwai.komai@gmail.coom>
2025-02-26 14:07:53 +1200

    adding icons

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,21 +5,26 @@
 
 import Link from 'next/link'
 import React from 'react'
+import { GrProjects } from 'react-icons/gr'
+import { IoHomeOutline } from 'react-icons/io5'
+import { MdOutlineAddAPhoto, MdWorkOutline } from 'react-icons/md'
+import { PiCertificate } from 'react-icons/pi'
+import { SlGraduation } from 'react-icons/sl'
 
 const Navbar = () => {
     const links = [
-        {label:'Home', href:'/'},
-        {label:'Education', href:'/education'},
-        {label:'Certification', href:'/certification'},
-        {label:'Experience', href:'/experience'},
-        {label:'Project', href:'/project'},
-        {label:'Photo', href:'/photo'},
+        {label:'Home', href:'/',icon:<IoHomeOutline />},
+        {label:'Education', href:'/education', icon: <SlGraduation />},
+        {label:'Certification', href:'/certification', icon: <PiCertificate />},
+        {label:'Experience', href:'/experience', icon:<MdWorkOutline />},
+        {label:'Project', href:'/project', icon: <GrProjects />},
+        {label:'Photo', href:'/photo', icon:<MdOutlineAddAPhoto />},
     ]
   return (
     <nav className='border-r h-lvh'>
         <ul className='mt-10'>
             {links.map(link=>
-            <Link className='flex h-10 hover:bg-zinc-200 items-center p-5 transition-colors' href={link.href} key={link.href}>{link.label}</Link>
+            <Link className='flex h-10 hover:bg-purple-500 items-center p-5 transition-colors gap-2' href={link.href} key={link.href}>{link.icon}{link.label}</Link>
             )}
         </ul>
     </nav>
