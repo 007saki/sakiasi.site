b715a40e8057ed7e8b901cdb9bc8ecd3e578327a
007saki <saki.baleiwai.komai@gmail.coom>
2025-02-28 19:16:43 +1200

    changing styling for timeline

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -54,7 +54,7 @@ const Education = () => {
         >
           <h3 className="vertical-timeline-element-title">Completed Online Certificate</h3>
           <h4 className="text-sm italic">LinkedIn Learning</h4>
-          <ol className='list-decimal px-10 mb-2'>
+          <ol className='list-decimal px-10'>
             <li>IT Support</li>
             <li>Cybersecurity</li>
             <li>Cybersecurity with Cloud Computing</li>
