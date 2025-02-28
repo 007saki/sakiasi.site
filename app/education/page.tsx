35a8b496ca3dc8589cb1adf36c4363056e671169
007saki <saki.baleiwai.komai@gmail.coom>
2025-02-28 20:48:33 +1200

    added new styling looks good now

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -5,7 +5,6 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import 'react-vertical-timeline-component/style.min.css';
 import Image from 'next/image';
 import { FaLinkedin } from 'react-icons/fa';
-import { MdStar } from 'react-icons/md';
 
 
 const Education = () => {
@@ -13,10 +12,10 @@ const Education = () => {
     <div>
       <VerticalTimeline>
         <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+            contentStyle={{ background: 'rgb(33, 150, 243)'}}
             date="2025 - present"
+            className="vertical-timeline-element--work"
+            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', }}
             icon={<Image className='rounded-xl' alt='sakiasi' src={'/sakiasi.JPG'} width={100} height={100} />}
           >
@@ -30,51 +29,51 @@ const Education = () => {
             </p>
         </VerticalTimelineElement>
 
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2024"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', }}
-            icon={<FaLinkedin />}
-          >
-            <h3 className="vertical-timeline-element-title">Completed Online Certificate</h3>
-            <h4 className="text-sm italic">LinkedIn Learning</h4>
-            <ol className='list-decimal px-10 mb-2'>
-              <li>Full-Stack Web Developer</li>
-              <li>JavaScript Essential</li>
-              <li>Learning ECMAScript 6+</li>
-              <li>Learning REST APIs</li>
-              <li>React Essential</li>
-            </ol>
-          </VerticalTimelineElement>
+        <VerticalTimelineElement
+          contentStyle={{ background: 'rgb(33, 150, 243)'}}
+          className="vertical-timeline-element--work"
+          date="2024"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', }}
+          icon={<FaLinkedin />}
+        >
+          <h3 className="vertical-timeline-element-title">Completed Online Certificate</h3>
+          <h4 className="text-sm italic">LinkedIn Learning</h4>
+          <ol className='list-decimal px-10 mb-2'>
+            <li>Full-Stack Web Developer</li>
+            <li>JavaScript Essential</li>
+            <li>Learning ECMAScript 6+</li>
+            <li>Learning REST APIs</li>
+            <li>React Essential</li>
+          </ol>
+        </VerticalTimelineElement>
 
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2023"
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', }}
-            icon={<FaLinkedin />}
-          >
-            <h3 className="vertical-timeline-element-title">Completed Online Certificate</h3>
-            <h4 className="text-sm italic">LinkedIn Learning</h4>
-            <ol className='list-decimal px-10 mb-2'>
+        <VerticalTimelineElement
+          contentStyle={{ background: 'rgb(33, 150, 243)'}}
+          className="vertical-timeline-element--work"
+          date="2023"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff', }}
+          icon={<FaLinkedin />}
+        >
+          <h3 className="vertical-timeline-element-title">Completed Online Certificate</h3>
+          <h4 className="text-sm italic">LinkedIn Learning</h4>
+          <ol className='list-decimal md:p-2'>
             <li>IT Support</li>
-              <li>Cybersecurity</li>
-              <li>Cybersecurity with Cloud Computing</li>
-              <li>Ethics in Information Security</li>
-              <li>IT Security Foundations Network Security</li>
-              <li>IT Security Foundations Operating System Security</li>
-              <li>IT Service Management</li>
-              <li>Learning Threat Modeling for Security Professionals</li>
-              <li>Network Administration</li>
-              <li>Networking Foundations</li>
-              <li>Project Management Foundations</li>
-              <li>Windows 10 for IT Support</li>
-              <li>Network Security</li>
-              <li>Project Management</li>
-            </ol>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-            icon={<MdStar />}/>
+            <li>Cybersecurity</li>
+            <li>Cybersecurity with Cloud Computing</li>
+            <li>Ethics in Information Security</li>
+            <li>IT Security Foundations Network Security</li>
+            <li>IT Security Foundations Operating System Security</li>
+            <li>IT Service Management</li>
+            <li>Learning Threat Modeling for Security Professionals</li>
+            <li>Network Administration</li>
+            <li>Networking Foundations</li>
+            <li>Project Management Foundations</li>
+            <li>Windows 10 for IT Support</li>
+            <li>Network Security</li>
+            <li>Project Management</li>
+          </ol>
+        </VerticalTimelineElement>
+
       </VerticalTimeline>
     </div>
   )
