59007e6222d0bb9aec4d8ef41cc00024a3963aae
007saki <saki.baleiwai.komai@gmail.coom>
2025-03-06 10:32:57 +1200

    styling cert form

--- app/cerSchema.ts
+++ app/cerSchema.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 
-
 export const cerSchema = z.object({
     title: z.string().min(1,'Required').max(255),
     start_time: z.string().min(1).max(255),
--- app/certification/page.tsx
+++ app/certification/page.tsx
@@ -1,48 +1,63 @@
 
-'use client'
-import { Button, Card, TextField } from '@radix-ui/themes';
-import { SubmitHandler, useForm } from "react-hook-form";
-import { z } from 'zod';
-import { cerSchema } from '../cerSchema';
-import { zodResolver } from '@hookform/resolvers/zod';
-import axios from 'axios';
 
-export type myType = z.infer<typeof cerSchema>;
+'use client'
+import { Button, Select, Text, TextField } from '@radix-ui/themes'
+import React, { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { cerSchema } from '../cerSchema'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+
+type cert = z.infer<typeof cerSchema>
 
 const Certificate = () => {
+  const [error, setError] = useState('')
+  const {register,handleSubmit,control,formState:{errors}} = useForm<cert>({
+    resolver: zodResolver(cerSchema)
+  })
 
-  const {register,handleSubmit} = useForm<myType>(
-    {resolver:zodResolver(cerSchema)}
-  )
-
-  const onSubmit: SubmitHandler<myType> = async (data) => {
-    
-    const formattedData = {
-        ...data,
-        start_time: new Date(data.start_time + 'T00:00:00.000Z').toISOString(),
-        end_time: new Date(data.end_time + 'T00:00:00.000Z').toISOString(),
-    };
-
-    console.log("Sending:", formattedData);  // See final data in console
-
-    await axios.post('/api/certificate', formattedData);
-};
+  const onSubmit=async(data)=>{
+    // console.log(data)
+    try {
+      await axios.post('/api/certificate',data)
+    } catch (error) {
+      setError(`${error}`)
+    }
+  }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='gap-5 flex flex-col items-center justify-center h-screen'>
-      <Card variant='surface' className='flex items-center justify-center md:w-1/3 sm:w-full' >
-      <div className=' w-full flex flex-col gap-5'>
-        <TextField.Root {...register('title')} placeholder='Enter Title' variant='soft' />
-        <TextField.Root type='date'  {...register('start_time')} placeholder='Start Time' variant='soft' />
-        <TextField.Root type='date'  {...register('end_time')} placeholder='End Time' variant='soft' />
-        <TextField.Root {...register('status')} placeholder='Status' variant='soft' />
+    <form onSubmit={handleSubmit(onSubmit)} className=' items-center justify-center flex w-full h-screen'>
+      <div className=' w-2/4 gap-5 flex flex-col'>
+      
+        <TextField.Root size={'3'} {...register('title')} variant='soft' placeholder='Enter Title' />
+        {errors.title&& <Text color='red'>{errors.title.message}</Text> }
+
+        <TextField.Root size={'3'} type='date' {...register('start_time')} variant='soft' placeholder='Start' />
+        {errors.start_time&& <Text color='red'>{errors.start_time.message}</Text> }
+
+        <TextField.Root size={'3'} type='date' {...register('end_time')} variant='soft' placeholder='End' />
+        {errors.end_time&& <Text color='red'>{errors.end_time.message}</Text> }
+
+        <Controller
+          control={control}
+          name='status'
+          render={({field})=>(
+            <Select.Root value={field.value} size={'3'} onValueChange={field.onChange}>
+              <Select.Trigger variant='soft' radius='medium' placeholder='Select Status'/>
+              <Select.Content>
+                <Select.Item value='COMPLETE'>Complete</Select.Item>
+                <Select.Item value='IN_PROGRESS'>In Progress</Select.Item>
+              </Select.Content>
+              {errors.status&& <Text color='red'>{errors.status.message}</Text> }
+            </Select.Root>
+          )}
+        />
+        
+        <Button type='submit'>Add Certification</Button>
       </div>
-      </Card>
-      <Button>Add Certificate</Button>
     </form>
   )
 }
-
-
 
 export default Certificate
