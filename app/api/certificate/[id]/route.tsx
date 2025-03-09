327579f33c996a9ea4937cd5467cec2d76ac4e53
007saki <saki.baleiwai.komai@gmail.coom>
2025-03-10 07:04:10 +1200

    remove route [id]

--- app/api/certificate/[id]/route.tsx
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@/prisma/client"
-import { NextResponse } from "next/server"
-
-
-export const GET=({params}:{params:{id:string}})=>{
-   const certificate = prisma.certificate.findUnique({where:{id:parseInt(params.id)}})
-   NextResponse.json(certificate)
-   console.log(certificate)
-}
