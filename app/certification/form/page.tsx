7651dc481e89c5717160477f61032fc1ae5a91b0
007saki <saki.baleiwai.komai@gmail.coom>
2025-03-09 19:09:50 +1200

    adding react hook form in certificate

--- app/certification/form/page.tsx
+++ app/certification/form/page.tsx
@@ -1,25 +1,42 @@
 
 
+'use client'
+import { certificateSchema } from '@/app/cerSchema'
+import { Button, Select, TextArea, TextField, Tooltip } from '@radix-ui/themes'
+import {Controller, useForm} from 'react-hook-form'
+import { z } from 'zod'
 
-import { Select, TextArea, TextField, Tooltip } from '@radix-ui/themes'
+type certType = z.infer<typeof certificateSchema>
 
 const CertificateForm = () => {
 
-  return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-        <div className='w-2/5 gap-3 flex flex-col'>
-        <Tooltip content='Certificate Date'>
-            <TextField.Root size={'3'} color='purple' variant='soft' type='date' />
-        </Tooltip>
-            <TextField.Root size={'3'} color='purple' placeholder='Institution' variant='soft' type='text' />
-            <TextField.Root size={'3'} color='purple' placeholder='Qualification' variant='soft' type='text' />
+    const {register, handleSubmit, control} = useForm<certType>()
 
-            <Select.Root size={'3'}>
+    const onSubmit=(data:certType)=>{
+        console.log(data)
+    }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center justify-center h-screen'>
+
+        <div className='md:w-2/5 gap-3 flex flex-col'>
+
+            <Tooltip content='Certificate Date'>
+                <TextField.Root {...register('date')} size={'3'} color='purple' variant='soft' type='date' />
+            </Tooltip>
+
+            <TextField.Root {...register('institution')} size={'3'} color='purple' placeholder='Institution' variant='soft' type='text' />
+            <TextField.Root {...register('qualificationType')} size={'3'} color='purple' placeholder='Qualification Type' variant='soft' type='text' />
+            <Controller
+            control={control}
+            name='category'            
+            render={({field}) =>
+            <Select.Root value={field.value} onValueChange={field.onChange} size={'3'}>
                 <Tooltip content='Certificate Category'>
                     <Select.Trigger color='purple' placeholder='Select Category...' />
                 </Tooltip>
-                <Select.Content variant='soft'>
-                    <Select.Group>
+                <Select.Content  variant='soft'>
+                    <Select.Group >
                         <Select.Label>Certificate Category</Select.Label>
                         <Select.Item value="WEB_DEVELOPMENT">Web Development</Select.Item>
                         <Select.Item value="CYBER_SECURITY">Cyber Security</Select.Item>
@@ -31,9 +48,14 @@ const CertificateForm = () => {
                         <Select.Item value="UI_UX_DESIGN">UI & UX Design</Select.Item>
                     </Select.Group>
                 </Select.Content>
-            </Select.Root>
+            </Select.Root>}
+            />
 
-            <Select.Root size={'3'}>
+            <Controller
+            control={control}
+            name='status'
+            render={({field})=>
+            <Select.Root value={field.value} onValueChange={field.onChange} size={'3'}>
                 <Tooltip content='Certificate Status'>
                     <Select.Trigger placeholder='Select Status...' />
                 </Tooltip>
@@ -44,13 +66,15 @@ const CertificateForm = () => {
                         <Select.Item value="IN_PROGRESS">In Progress</Select.Item>
                     </Select.Group>
                 </Select.Content>
-            </Select.Root>
+            </Select.Root>}
+            />
+            <TextArea {...register('certificate_desc')} size={'3'} color='purple' placeholder='Enter Certificate Description' variant='soft'/>
+            <TextField.Root {...register('name')} size={'3'} color='purple' placeholder='Enter Name' variant='soft' type='text'/>
 
-            <TextArea size={'3'} color='purple' placeholder='Enter Certificate Description' variant='soft'/>
-            <TextField.Root size={'3'} color='purple' placeholder='Enter Name' variant='soft' type='text'/>
             <input type="file"/>
         </div>
-    </div>
+        <Button variant='soft' type='submit'>Save</Button>
+    </form>
   )
 }
 
