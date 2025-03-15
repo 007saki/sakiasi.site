1b0457973a15e9be6b35614c290752e5ab4c9d1f
007saki <saki.baleiwai.komai@gmail.coom>
2025-03-15 18:21:07 +1200

    added experience card and styling it

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -4,6 +4,7 @@
 import { prisma } from '@/prisma/client'
 import { Box, Card, Flex, Text } from '@radix-ui/themes'
 import Image from 'next/image'
+import { SlCalender } from 'react-icons/sl'
 import Markdown from 'react-markdown'
 
 const ExperiencePage = async() => {
@@ -12,16 +13,15 @@ const ExperiencePage = async() => {
   return (
     <div className='flex flex-col gap-3 items-center py-5'>
         {experiences.map(experience=>
-            <Box key={experience.id} className='w-3/5'>
+            <Box key={experience.id} className='md:w-3/5 p-2'>
                 <Card className=' flex flex-col justify-center'>
                     <Flex gap="3" align="center" className='flex justify-center'>
                         <Image
-                            height={50}
-                            width={50}
+                            height={100}
+                            width={100}
                             alt='Logo'
                             style={{width:50 , height:50}}
                             src={`https://drive.google.com/uc?id=${experience?.employer_logo}`}
-                            // fallback={experience.company}
                         />
                         <Box>
                             <Text className='text-2xl' as="div" size="2" weight="bold">
@@ -33,14 +33,19 @@ const ExperiencePage = async() => {
                         </Box>
                     </Flex>
 
-                    <div className='flex flex-col items-center gap-1'>
-                        <Text as="div" size="2" color="gray">From: {experience.startDate.toDateString()}</Text>
-                        <Text as="div" size="2" color="gray">To: {experience.endDate?.toDateString()}</Text>
+                    <div className='flex mt-5 gap-1'>
+                        <div><SlCalender size={'40'} color='purple' /></div>
+                        <div>
+                            <Text as="div" size="2" color="gray">From: {experience.startDate.toDateString()}</Text>
+                            <Text as="div" size="2" color="gray">To: {experience.endDate?.toDateString()}</Text>
+                        </div>
                     </div>
                     <div className='prose mt-5 border-t'>
-                        <Markdown>
-                            {experience.description}
-                        </Markdown>
+                        <div className='mt-5'>
+                            <Markdown>
+                                {experience.description}
+                            </Markdown>
+                        </div>
                     </div>
                 </Card>
             </Box>
