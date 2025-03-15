62801eadd4742f52dbab66c8e0ff4ca978113769
007saki <saki.baleiwai.komai@gmail.coom>
2025-03-15 15:16:47 +1200

    remaking qualification details page

--- app/qualification/[id]/page.tsx
+++ app/qualification/[id]/page.tsx
@@ -3,7 +3,10 @@
 
 
 import { prisma } from '@/prisma/client'
-import React from 'react'
+import { Badge, Text } from '@radix-ui/themes'
+import Image from 'next/image'
+import { FaCertificate } from 'react-icons/fa'
+import ReactMarkdown from 'react-markdown'
 
 const QualificationDetails = async({params}:{params:Promise<{id:string}>}) => {
     const id = (await params).id
@@ -11,11 +14,37 @@ const QualificationDetails = async({params}:{params:Promise<{id:string}>}) => {
     const qualification = await prisma.certificate.findUnique({where:{id:(parseInt(id))}})
 
   return (
-    <div>
-        <p>Name: {qualification?.name}</p>
-        <p>Category: {qualification?.category}</p>
-        <p>Description: {qualification?.certificate_desc}</p>
+    <div className='p-4'>
+        <div className=' items-center flex flex-col gap-5' key={qualification!.id}>
+          <div className='flex items-center flex-col'>
+            <Image
+              width={400}
+              height={400}
+              src={`https://drive.google.com/uc?id=${qualification?.certificate_id}`}
+              alt={`${qualification!.name}`}
+            />
+            <div>
+              <div className='flex items-center flex-col'>
+                <div className='flex gap-4 items-center p-2'>
+                  <Text className='font-serif flex items-center gap-1'> <FaCertificate color='purple'/> {qualification?.name}</Text>
+                <div className='border-l px-3 flex items-center gap-2'>
+                  <Badge variant='soft' color='green'>{qualification!.status}</Badge>
+                </div>
+              </div>
+            </div>
+          </div>
+                <div className='flex items-center justify-center'>
+                  <Text className='italic'>{new Date(qualification!.date).toDateString()}</Text>
+                </div>
+          </div>
+          <div className=' prose border-t p-2 py-5'>
+            <ReactMarkdown>
+              {qualification!.certificate_desc}
+            </ReactMarkdown>
+          </div>
+        </div>
     </div>
+
   )
 }
 
